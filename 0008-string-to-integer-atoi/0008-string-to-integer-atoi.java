e1b0e5a6241fb262e3b6a275fa17ea6d9c6ca7e0
Shubham Kumar Ray <150553890+ShubhamKRay@users.noreply.github.com>
2025-12-11 15:12:36 +0530

    Time: 1 ms (99.95%), Space: 43.8 MB (33.25%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,21 +1,26 @@
 class Solution {
     public int myAtoi(String s) {
-        int i=0, ans=0, sign=1;
-        while(i<s.length() && s.charAt(i)==' ') i++;
-        if(i==s.length())    return 0;
         
-        if(s.charAt(i)=='-'){
-            sign=-1;
+        int i=0, ans=0, sign=1;
+        while(i<s.length() && s.charAt(i) ==' ')
+        i++;
+        if(i==s.length())
+        return 0;
+        if(s.charAt(i) == '-'){
+            sign = -1;
             i++;
-        }else if(s.charAt(i)=='+'){
+
+        } else if(s.charAt(i)=='+'){
             i++;
         }
         while(i<s.length() && Character.isDigit(s.charAt(i))){
             int number = s.charAt(i) - '0';
             i++;
-            if(ans>Integer.MAX_VALUE / 10 || (ans==Integer.MAX_VALUE / 10 && number >               Integer.MAX_VALUE % 10)){
-                if(sign == -1 )  return Integer.MIN_VALUE;
-                else return Integer.MAX_VALUE;
+            if(ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && number > Integer.MAX_VALUE %10)){
+                if(sign == -1)
+                return Integer.MIN_VALUE;
+                else
+                return Integer.MAX_VALUE;
             }
             ans = ans * 10 + number;
         }
