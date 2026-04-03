c42ace98db21d3fbf229a8bd91d00ca83ddb645d
Shubham Kumar Ray <150553890+ShubhamKRay@users.noreply.github.com>
2026-04-04 01:23:44 +0530

    Time: 1 ms (99.98%), Space: 47.1 MB (36.89%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,7 +3,7 @@ class Solution {
         
         int ans = 0;
         int n = nums.length;
-        for(int i=0; i<=n; i++){
+        for(int i=0; i<n; i++){
             ans = ans ^ nums[i];
         }
         return ans;
